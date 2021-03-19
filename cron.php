9eff1c3cf9a6cd7d6d79ae087938f5bbed9e46c6
sim1-dev <simonetenisci10@gmail.com>
2021-03-19 21:43:44 +0100

    first commit

---
+++ cron.php
@@ -0,0 +1,21 @@
+<?php
+
+$bot_token = "1717660927:AAH-mr5L77Ae2WbHdWDySmabO2hrunsAyLc";
+$channel_id = "-1001136654503";
+
+sendGETMessage(date("H:i:s"));
+
+function sendGETMessage($message) {
+	global $bot_token, $channel_id;
+    $url = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$channel_id&text=$message";
+    $options = array(
+        'http'=>array(
+            'method'=>"POST",
+            'header'=>"Accept-language: en\r\n" .
+            "Cookie: foo=bar\r\n" .
+            "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n"
+	    )
+	);
+	$context = stream_context_create($options);
+	file_get_contents($url, false, $context);
+}
