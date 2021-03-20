f62f6fc9aea9b7fd39bec17ac831b4bc43fcdd78
sim1-dev <simonetenisci10@gmail.com>
2021-03-20 15:48:54 +0100

    store municipalities test

--- execute.php
+++ execute.php
@@ -2,6 +2,7 @@
 
 require_once("db.php");
 require_once("Settings.php");
+require_once("Municipalities.php");
 
 //APP VARS
 
@@ -15,12 +16,13 @@ $db_name = getenv("DB_NAME");
 global $username, $settings, $active_setting;
 
 $settings = new Settings($db_driver, $db_host, $db_port, $db_user, $db_password, $db_name);
+$municipalities = new Municipalities($db_driver, $db_host, $db_port, $db_user, $db_password, $db_name);
 
 $active_setting = $settings->getActiveSetting();
 
 $response = '';
-$channel_id = getenv("CHANNEL_ID");
-$bot_token = getenv("BOT_TOKEN");
+$channel_id = (string)getenv("CHANNEL_ID");
+$bot_token = (string)getenv("BOT_TOKEN");
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -32,11 +34,125 @@ if(!$update)
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$username = $message["from"]["username"];
+$username = (string)$message["from"]["username"];
 $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
+
+//ADMIN COMMANDS SECTION
+
+if(strpos($text, "/start") === 0)
+{
+    global $username;
+    if($username == "TeamBallo") {
+        sendGETMessage($active_setting);
+    } else {
+        sendGETMessage("test");
+    }
+}
+
+if(strpos($text, "/comunerandom") === 0)
+{
+    global $username, $municipalities;
+    if($username == "TeamBallo") {
+        $random = $municipalities->getRandomMunicipality();
+        sendGETMessage($random["name"]);
+    } else {
+        sendGETMessage("[ER] Non hai i permessi per accedere a questo comando.");
+    }
+}
+
+if(strpos($text, "/broadcast") === 0)
+{
+    global $username;
+    if($username == "TeamBallo") {
+        sendGETMessageToChannel(str_replace("/broadcast ","",$text));
+    } else {
+        sendGETMessage("[ER] Non hai i permessi per accedere a questo comando.");
+    }
+}
+
+if(strpos($text, "/lancia") === 0)
+{
+    global $username, $active_setting, $settings, $municipalities;
+    $app_running = $active_setting["app_running"];
+    if($username == "TeamBallo") {
+        if($app_running == 0) {
+            initGuerra(1);
+            sendGETMessage("[OK] Nuova guerra avviata!");
+            sendGETMessage("NUOVA GUERRA AVVIATA, PREPARARSI ALLA BATTAGLIA!");
+        } else {
+            sendGETMessage("[NO] Guerra già in esecuzione!");
+        }
+    } else {
+        sendGETMessage("[ER] Non hai accesso a questo comando.");
+    }
+}
+
+if(strpos($text, "/arresta") === 0)
+{
+    global $username, $active_setting, $settings, $municipalities;
+    $app_running = $active_setting["app_running"];
+    if($username == "TeamBallo") {
+        if($app_running == 1) {
+            initGuerra(0);
+            sendGETMessage("[OK] Guerra terminata forzatamente!");
+            sendGETMessageToChannel("GUERRA TERMINATA FORZATAMENTE DALL'AMMINISTRATORE!");
+        } else {
+            sendGETMessage("[NO] Guerra non in esecuzione!");
+        }
+    } else {
+        sendGETMessage("[ER] Non hai accesso a questo comando.");
+    }
+}
+
+
+if(strpos($text, "/riavvia") === 0)
+{
+    global $username, $active_setting, $settings;
+    $app_running = $active_setting["app_running"];
+    if($username == "TeamBallo") {
+        if($app_running == 0) {
+            $settings->updateSettingRunning($active_setting["id"], 1);
+            sendGETMessage("[OK] Guerra avviata!");
+        } else {
+            sendGETMessage("[NO] Guerra già in esecuzione!");
+        }
+    } else {
+        sendGETMessage("[ER] Non hai accesso a questo comando.");
+    }
+}
+
+if(strpos($text, "/pausa") === 0)
+{
+    global $username, $active_setting, $settings;
+    $app_running = $active_setting["app_running"];
+    if($username == "TeamBallo") {
+        if($app_running == 1) {
+            $settings->updateSettingRunning($active_setting["id"], 0);
+            sendGETMessage("[OK] Guerra fermata!");
+        } else {
+            sendGETMessage("[NO] Guerra già ferma!");
+        }
+    } else {
+        sendGETMessage("[ER] Non hai accesso a questo comando.");
+    }
+}
+
+if(strpos($text, "/store") === 0)
+{
+    global $username, $municipalities;
+    if($username == "TeamBallo") {
+        $municipalities->storeMunicipalities();
+        sendGETMessage("Comuni inseriti");
+    } else {
+        sendGETMessage("[ER] Non hai i permessi per accedere a questo comando.");
+    }
+}
+
+
+//FUNCTIONS
 
 function sendGETMessage($message) {
 	global $bot_token, $chatId;
@@ -68,57 +184,11 @@ function sendGETMessageToChannel($message) {
 	file_get_contents($url, false, $context);
 }
 
-
-//ADMIN COMMANDS SECTION
-
-if(strpos($text, "/start") === 0)
-{
-    global $username;
-    if($username == "TeamBallo") {
-        sendGETMessage($active_setting);
-    } else {
-        sendGETMessage("test");
-    }
+function initGuerra($active) {
+    global $municipalities, $settings, $active_setting;
+    $municipalities->resetGuerra();
+    $settings->updateSettingRunning($active_setting["id"], $active);
 }
-
-if(strpos($text, "/broadcast") === 0)
-{
-    global $username;
-    if($username == "TeamBallo") {
-        sendGETMessageToChannel(str_replace("/broadcast ","",$text));
-    } else {
-        sendGETMessage("[ER] Non hai i permessi per accedere a questo comando.");
-    }
-}
-
-
-if(strpos($text, "/avvia") === 0)
-{
-    global $username, $active_setting, $settings;
-    $app_running = $active_setting["app_running"];
-    if($username == "TeamBallo") {
-        if($app_running == 0) {
-            sendGETMessage(implode(",", $active_setting));
-            $settings->updateSettingRunning(1, 1);
-            sendGETMessage("app_running: ".$app_running);
-            sendGETMessage("app_id: ".$active_setting["id"]);
-            sendGETMessage("[OK] Guerra avviata!");
-        } else {
-            sendGETMessage("[NO] Guerra già in esecuzione!");
-        }
-    } else {
-        sendGETMessage("[ER] Non hai accesso a questo comando.");
-    }
-}
-
-if(strpos($text, "/env") === 0)
-{
-    global $username, $settings;
-    sendGETMessage(getenv("CHANNEL_ID"));
-    sendGETMessage(getenv("BOT_TOKEN"));
-    sendGETMessage($settings->getActiveSetting()["app_running"]);
-}
-
 
 ?>
 
