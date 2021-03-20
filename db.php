d0745564591f1842cd63a5120f4cf880777b546d
sim1-dev <simonetenisci10@gmail.com>
2021-03-20 12:03:33 +0100

    entity test

--- db.php
+++ db.php
@@ -11,8 +11,9 @@ class DB {
         $db_username = getenv("DB_USER");
         $db_password = getenv("DB_PASSWORD");
         $db_name = getenv("DB_NAME");
+        $db_charset = 'utf8';
 
-        $dsn = "$db_driver:host=$db_host;dbname=$db_name;charset='utf8mb4'";
+        $dsn = "$db_driver:host=$db_host;dbname=$db_name;charset=$db_charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
