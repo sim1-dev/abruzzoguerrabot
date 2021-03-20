4fdcb0d6b9b876abdc9b586de3d37e07f845dca9
sim1-dev <simonetenisci10@gmail.com>
2021-03-20 17:33:33 +0100

    store municipalities test

--- Municipalities.php
+++ Municipalities.php
@@ -146,7 +146,7 @@ class Municipalities extends Entity {
 
     public function getRandomMunicipality() {
 
-        $result = $this->entity->query("SELECT * FROM municipalities WHERE weight>0 ORDER BY RANDOM() LIMIT 1"); //change to RAND() FOR MYSQL
+        $result = $this->entity->query("SELECT * FROM municipalities WHERE weight>0 ORDER BY random() LIMIT 1"); //change to RAND() FOR MYSQL
         return $result->execute();
 
     }
