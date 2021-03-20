c7037dd7b0713e11d81b64de6a4fdbcbbe2f924c
sim1-dev <simonetenisci10@gmail.com>
2021-03-20 12:26:22 +0100

    entity test

--- db.php
+++ db.php
@@ -1,19 +1,26 @@
 <?php
 
-class DB {
+class Entity {
 
-    protected $db_driver, $db_host, $db_username, $db_password, $db_name, $entity;
+    protected $db_driver;
+    protected $db_host;
+    protected $db_port;
+    protected $db_username;
+    protected $db_password;
+    protected $db_name;
+    protected $entity;
 
-    public function __construct() {
+    public function __construct($_driver, $_host, $_port, $_user, $_password, $_name) {
 
-        $this->db_driver = getenv("DB_DRIVER");
-        $this->db_host = getenv("DB_HOST");
-        $this->db_username = getenv("DB_USER");
-        $this->db_password = getenv("DB_PASSWORD");
-        $this->db_name = getenv("DB_NAME");
-        $this->db_charset = 'UTF8';
+        $this->db_driver = $_driver;
+        $this->db_host = $_host;
+        $this->db_port = $_port;
+        $this->db_user = $_user;
+        $this->db_password = $_password;
+        $this->db_name = $_name;
+        $db_charset = 'UTF8';
 
-        $dsn = "$this->db_driver:host=$this->db_host;dbname=$this->db_name;port=$this->db_port;charset=$this->db_charset";
+        $dsn = "$this->db_driver:host=$this->db_host;dbname=$this->db_name;port=$this->db_port;charset=$db_charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
