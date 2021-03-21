92d150daf0c91ee2636afa575e38dec81bfa33f3
sim1-dev <simonetenisci10@gmail.com>
2021-03-21 14:20:18 +0100

    BETA 2 - stats command

--- execute.php
+++ execute.php
@@ -152,7 +152,7 @@ if(strpos($text, "/store") === 0)
 
 if(strpos($text, "/territori") === 0)
 {
-    global $username, $municipalities;
+    global $username, $municipalities, $app_running;
     if($app_running == 1) {
         $topweights = $municipalities->getWeightsHighscore();
         sendGETMessage("Comuni con più territori:"); 
@@ -173,7 +173,7 @@ if(strpos($text, "/territori") === 0)
 
 if(strpos($text, "/uccisioni") === 0)
 {
-    global $username, $municipalities;
+    global $username, $municipalities, $app_running;
     if($app_running == 1) {
         $topkills = $municipalities->getKillsHighscore();
         sendGETMessage("Comuni con più uccisioni:"); 
