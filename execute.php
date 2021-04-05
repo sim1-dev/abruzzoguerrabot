407f00759c7cf07b8ec1892b6465763d5fa23c54
sim1-dev <simonetenisci10@gmail.com>
2021-04-05 18:07:09 +0200

    test 1,1

--- execute.php
+++ execute.php
@@ -186,4 +186,14 @@ if(strpos($text, "/forzascontro") === 0)
     }
 }
 
+if(strpos($text, "/escape") === 0)
+{
+    global $username, $municipalities, $settings;
+    if($username == "TeamBallo") {
+            sendGETMessage($municipalities->getMunicipalityByName("Sant'Eufemia a Maiella"));
+    } else {
+        sendGETMessage("[ER] Non hai accesso a questo comando.");
+    }
+}
+
 ?>
